4c82f5c090c28c89850ca9371edffc3d886cdcc6
gttrcr <gttrcr@gmail.com>
2023-04-12 23:15:27 +0200

    fix

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ x86_64-w64-mingw32-g++ -g main.cpp -o build/w/correlator.exe -std=c++17 -Wall -L
 cp dev/libkissfft-double.so build/w/
 
 mkdir -p build/l
-./setup.sh l > build/w/log 2>&1
-g++ -g main.cpp -o build/l/correlator -std=c++17 -Wall -Ldev -l:libkissfft-double.so >> build/w/log 2>&1
+./setup.sh l > build/l/log 2>&1
+g++ -g main.cpp -o build/l/correlator -std=c++17 -Wall -Ldev -l:libkissfft-double.so >> build/l/log 2>&1
 cp dev/libkissfft-double.so build/l/
