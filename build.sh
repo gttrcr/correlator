a8e3e846c596042fb1b8abdf5bc5d71567a83f7c
gttrcr <gttrcr@gmail.com>
2023-04-14 21:58:39 +0200

    zip folders

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 rm -r build
 
 echo "building for win platform..."
-mkdir -p build/w
-./setup.sh w > build/w/log 2>&1
-x86_64-w64-mingw32-g++ -g main.cpp -o build/w/correlator.exe -std=c++17 -Wall -Ldev -l:`ls dev/ | grep 'libkissfft-.*so$'` >> build/w/log 2>&1
-cp dev/`ls dev/ | grep 'libkissfft-.*so$'` build/w/
-if [ -z `ls build/w | grep correlator` ]; then
+mkdir -p build/win
+./setup.sh w > build/win/log 2>&1
+x86_64-w64-mingw32-g++ -g main.cpp -o build/win/correlator.exe -std=c++17 -Wall -Ldev -l:`ls dev/ | grep 'libkissfft-.*so$'` >> build/win/log 2>&1
+cp dev/`ls dev/ | grep 'libkissfft-.*so$'` build/win/
+if [ -z `ls build/win | grep correlator` ]; then
     echo "error on build for win"
 fi
 
 echo "building for linux platform..."
-mkdir -p build/l
-./setup.sh l > build/l/log 2>&1
-g++ -g main.cpp -o build/l/correlator -std=c++17 -Wall -Ldev -l:`ls dev/ | grep 'libkissfft-.*so$'` >> build/l/log 2>&1
-cp dev/`ls dev/ | grep 'libkissfft-.*so$'` build/l/
-if [ -z `ls build/l | grep correlator` ]; then
+mkdir -p build/linux
+./setup.sh l > build/linux/log 2>&1
+g++ -g main.cpp -o build/linux/correlator -std=c++17 -Wall -Ldev -l:`ls dev/ | grep 'libkissfft-.*so$'` >> build/linux/log 2>&1
+cp dev/`ls dev/ | grep 'libkissfft-.*so$'` build/linux/
+if [ -z `ls build/linux | grep correlator` ]; then
     echo "error on build for linux"
 fi
--- release.sh
+++ release.sh
@@ -1,23 +1,25 @@
 rm -r release
 
 echo "building for win platform..."
-mkdir -p release/w
-./setup.sh w > release/w/log 2>&1
-x86_64-w64-mingw32-g++ -O3 main.cpp -o release/w/correlator.exe -std=c++17 -Wall -Ldev -l:`ls dev/ | grep 'libkissfft-.*so$'` >> release/w/log 2>&1
-cp dev/`ls dev/ | grep 'libkissfft-.*so$'` release/w/
-if [ -z `ls release/w | grep correlator` ]; then
+mkdir -p release/win
+./setup.sh w > release/win/log 2>&1
+x86_64-w64-mingw32-g++ -O3 main.cpp -o release/win/correlator.exe -std=c++17 -Wall -Ldev -l:`ls dev/ | grep 'libkissfft-.*so$'` >> release/win/log 2>&1
+cp dev/`ls dev/ | grep 'libkissfft-.*so$'` release/win/
+if [ -z `ls release/win | grep correlator` ]; then
     echo "error on release for win"
 else
-    rm release/w/log
+    rm release/win/log
+    zip -r release/win.zip release/win
 fi
 
 echo "building for linux platform..."
-mkdir -p release/l
-./setup.sh l > release/l/log 2>&1
-g++ -O3 main.cpp -o release/l/correlator -std=c++17 -Wall -Ldev -l:`ls dev/ | grep 'libkissfft-.*so$'` >> release/l/log 2>&1
-cp dev/`ls dev/ | grep 'libkissfft-.*so$'` release/l/
-if [ -z `ls release/l | grep correlator` ]; then
+mkdir -p release/linux
+./setup.sh l > release/linux/log 2>&1
+g++ -O3 main.cpp -o release/linux/correlator -std=c++17 -Wall -Ldev -l:`ls dev/ | grep 'libkissfft-.*so$'` >> release/linux/log 2>&1
+cp dev/`ls dev/ | grep 'libkissfft-.*so$'` release/linux/
+if [ -z `ls release/linux | grep correlator` ]; then
     echo "error on release for linux"
 else
-    rm release/l/log
+    rm release/linux/log
+    zip -r release/linux.zip release/linux
 fi
